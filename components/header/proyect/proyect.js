d662905a52ff9e2b1752277dde65f6cd4d94a092
macalderas <macalderas@scl.edu.gt>
2024-09-24 09:12:09 -0600

    Header Portafolio personal

--- components/header/proyect/proyect.js
+++ components/header/proyect/proyect.js
@@ -1,1 +1,32 @@
-    
+    function proyect(){
+
+        let section = document.createElement('section');
+        section.appendChild(header()) 
+        section.appendChild(Item())
+          return section
+    }
+
+function header(){
+    let div = document.createElement('div');
+        div.className = "header";
+
+        let h2 = document.createElement('h2');
+        h2.innerHTML = "proyectos ";
+        div.appendChild(h2);
+
+
+        let btn = document.createElement('div');
+        btn.innerHTML = "git hub";
+        div.appendChild(btn);
+
+    return div ;
+}
+
+    function Item(){
+        let div = document.createElement('div')
+        div.innerText = "item de lista";
+
+        return div ;
+    }
+
+    export {proyect}
